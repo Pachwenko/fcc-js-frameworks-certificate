b8cc3ba409a44391e7177fd9ad36972a4872cd77
Pachwenko <pmccart4096@gmail.com>
2021-11-13 21:31:53 -0600

    clean up imports

--- src/components/DrumMachine.jsx
+++ src/components/DrumMachine.jsx
@@ -1,9 +1,7 @@
-import { useState, useEffect } from "react";
-import { Provider } from "react-redux";
+import { useEffect } from "react";
+import { useSelector, useDispatch, Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import { createSlice } from "@reduxjs/toolkit";
-
-import { useSelector, useDispatch } from "react-redux";
 
 const displayTextSlice = createSlice({
   name: "displayText",
