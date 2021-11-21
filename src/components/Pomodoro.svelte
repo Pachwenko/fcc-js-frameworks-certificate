cea3aec01db2dac5879f1cc8a948e9ae0df7265b
Pachwenko <32424503+Pachwenko@users.noreply.github.com>
2021-11-20 18:14:47 -0600

    finish pomodoro logic

--- src/components/Pomodoro.svelte
+++ src/components/Pomodoro.svelte
@@ -1,19 +1,45 @@
 <script>
     import { breakLength, sessionLength } from './store.js';
+    const SESSION_MODE = "SESSION";
+    const BREAK_MODE = "BREAK";
 
     let minutes = "25";
     let seconds = "00";
+    let current_mode = SESSION_MODE;
     let inProgress = false;
     let timerFinished;
     let timerInterval;
 
     $: if (!inProgress) { minutes = $sessionLength };
+    $: if (minutes === 0 && seconds === 0) { playSound() };
+
+    function playSound() {
+        stopTimer();
+        document.getElementById("beep").play();
+        if (current_mode === SESSION_MODE) {
+            current_mode = BREAK_MODE;
+            minutes = $breakLength;
+        } else if (current_mode === BREAK_MODE) {
+            current_mode = SESSION_MODE;
+            minutes = $sessionLength;
+        }
+        calculateEndTime();
+        tickTimer();
+    }
+
+    function stopSound() {
+        document.getElementById("beep").currentTime = 0;
+        document.getElementById("beep").pause();
+    }
 
     function reset() {
         breakLength.reset();
         sessionLength.reset();
         stopTimer();
         inProgress = false;
+        stopSound();
+        minutes = current_mode === SESSION_MODE ? $sessionLength : $breakLength;
+        seconds = "00"
     }
 
     function tickTimer() {
@@ -22,19 +48,25 @@
         seconds = time.getSeconds();
     }
 
-    function startTimer(minutes) {
+    function calculateEndTime() {
         timerFinished = new Date();
-        timerFinished.setMinutes(timerFinished.getMinutes() + $sessionLength)
+        let length = current_mode === SESSION_MODE ? $sessionLength : $breakLength;
+        timerFinished.setMinutes(timerFinished.getMinutes() + length)
+    }
+
+    function startTimer() {
+        stopSound();
+        calculateEndTime();
         timerInterval = setInterval(tickTimer, 1000);
         inProgress = true;
     }
 
-    function stopTimer(minutes) {
+    function stopTimer() {
         clearInterval(timerInterval);
+        timerInterval = null;
     }
 
     function toggle() {
-        console.log(timerInterval);
         if (timerInterval) {
             stopTimer();
         } else {
@@ -46,28 +78,29 @@
 <div
   class="mx-auto my-10 p-10 md:w-1/2 text-center justify-center"
 >
-<h1 id="break-label">Break length: {$breakLength}</h1>
-<button on:click={breakLength.decrement} id="break-decrement">
-	-
-</button>
-<button on:click={breakLength.increment} id="break-increment">
-	+
-</button>
-<br />
-<h1 id="session-label">Session Length: {$sessionLength}</h1>
-<button on:click={sessionLength.decrement} id="session-decrement">
-	-
-</button>
-<button on:click={sessionLength.increment} id="session-increment">
-	+
-</button>
-<br />
-<h2 id="timer-label">Session</h2>
-<h2 id="time-left">{minutes}:{seconds}</h2>
-<button on:click={toggle} id="start_stop">
-	Start/Stop
-</button>
-<button on:click={reset} id="reset">
-	Reset
-</button>
+    <h1 id="break-label">Break length: {$breakLength}</h1>
+    <button on:click={breakLength.decrement} id="break-decrement">
+        -
+    </button>
+    <button on:click={breakLength.increment} id="break-increment">
+        +
+    </button>
+    <br />
+    <h1 id="session-label">Session Length: {$sessionLength}</h1>
+    <button on:click={sessionLength.decrement} id="session-decrement">
+        -
+    </button>
+    <button on:click={sessionLength.increment} id="session-increment">
+        +
+    </button>
+    <br />
+    <h2 id="timer-label">{current_mode}</h2>
+    <h2 id="time-left">{minutes}:{seconds}</h2>
+    <button on:click={toggle} id="start_stop">
+        Start/Stop
+    </button>
+    <button on:click={reset} id="reset">
+        Reset
+    </button>
+    <audio id="beep" src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav" />
 </div>
