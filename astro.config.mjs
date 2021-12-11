28d69084a7bb3fb504bc9210d5aa73778095324a
Pachwenko <32424503+Pachwenko@users.noreply.github.com>
2021-12-11 14:01:16 -0600

    Revert "add back github pages code"
    This reverts commit c95df3d2158bb37145731bdb0ab98191753355be.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 export default ({
-  buildOptions: {
-    site: 'https://pachwenko.github.io/fcc-js-frameworks-certificate/',
-  },
   renderers: ['@astrojs/renderer-react', '@astrojs/renderer-svelte'],
   devOptions: {
     tailwindConfig: './tailwind.config.js',
