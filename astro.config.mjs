7be37c3120d16b2b8a091570301303b34fceac3f
Pachwenko <32424503+Pachwenko@users.noreply.github.com>
2022-01-16 11:00:15 -0600

    update packages and add compression step

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
 export default ({
+  buildOptions: {
+    site: 'https://fcc-js-frameworks-certificate.patrickmccartney.dev'
+  },
   renderers: ['@astrojs/renderer-react', '@astrojs/renderer-svelte'],
   devOptions: {
     tailwindConfig: './tailwind.config.js',
   },
+  vite: {
+    build: {
+      minifiy: 'esbuild',
+      reportCompressedSize: true
+    }
+  }
 });
